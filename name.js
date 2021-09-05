dc34ebe3c375255d9b97546eb893231f10043b2d
elangyudha280 <elangyudharakasiwi@gmail.com>
2021-09-05 20:16:07 +0700

    begin project

---
+++ name.js
@@ -0,0 +1,21 @@
+/*
+alert('selamat datang');
+
+    var nama = prompt('masukan nama');
+    alert('hallo ' +  nama);
+
+alert('terima kasih sudah mengunjungi web kami');
+*/
+
+/*
+var angka =prompt('masukan angka:');
+if( angka % 2  === 0){
+    alert(angka + ' adalah bilangan genap');
+}
+else if(angka % 2 === 1){
+    alert(angka + 'adalah bilangan ganjil');
+}
+else{
+    alert('yang anda masukan bukan angka');
+}
+*/
